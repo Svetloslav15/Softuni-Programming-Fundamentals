21b881bae19562780573b7a6fb2f76dc8c65db06
Svetloslav15 <Sn896215>
2018-04-06 22:43:42 +0300

    add exam

---
+++ Exam 04 March/01.Padawan Equipment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Problem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal amountOfMoney = decimal.Parse(Console.ReadLine());
+            int numberOfStudents = int.Parse(Console.ReadLine());
+            decimal priceOfLightSabers = decimal.Parse(Console.ReadLine());
+            decimal priceOfRobes = decimal.Parse(Console.ReadLine());
+            decimal priceOfBelts = decimal.Parse(Console.ReadLine());
+
+            int freeBelts = numberOfStudents / 6;
+            decimal allMoney = (priceOfLightSabers * Math.Ceiling(numberOfStudents + 0.1m * numberOfStudents)) + priceOfRobes * numberOfStudents + priceOfBelts * (numberOfStudents - freeBelts);
+
+            if (allMoney <= amountOfMoney)
+            {
+                Console.WriteLine($"The money is enough - it would cost {allMoney:F2}lv.");
+            }
+            else
+            {
+                decimal neededMoney = allMoney - amountOfMoney;
+                Console.WriteLine($"Ivan Cho will need {neededMoney:F2}lv more.");
+            }
+        }
+    }
+}
---
+++ Exam 04 March/02. Kamino Factory/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Kamino_Factory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+            int[] result = new int[length];
+
+            int bestLength = 0;
+            int bestIndex = 0;
+            int counter = 1;
+            int seqIndex = 1;
+
+            string input = Console.ReadLine();
+            while (input != "Clone them!")
+            {
+                int[] temp = input.Split(new char[] { '!' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                int tempLength = 0;
+                int tempIndex = 0;
+
+                for (int index = 0; index < temp.Length; index++)
+                {
+                    if (temp[index] == 1)
+                    {
+                        tempLength++;
+                        if (tempLength > bestLength)
+                        {
+                            result = temp;
+                            bestLength = tempLength;
+                            bestIndex = tempIndex;
+                            seqIndex = counter;
+                        }
+                        else if (tempLength == bestLength)
+                        {
+                            if (tempIndex < bestIndex)
+                            {
+                                result = temp;
+                                bestLength = tempLength;
+                                bestIndex = tempIndex;
+                                seqIndex = counter;
+                            }
+
+                            else if (tempIndex == bestIndex && temp.Sum() > result.Sum())
+                            {
+                                result = temp;
+                                bestLength = tempLength;
+                                bestIndex = tempIndex;
+                                seqIndex = counter;
+                            }
+                        }                    
+                    }
+                else
+                {
+                    tempLength = 0;
+                    tempIndex = index + 1;
+                }
+                }
+                counter++;
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Best DNA sample {seqIndex} with sum: {result.Sum()}.");
+            Console.WriteLine(string.Join(" ",result));
+        }
+    }
+}
